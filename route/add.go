e2760d362651bd6a23f07b804cbe786472ae4f9f
最毕设 <liyuliang2013@gmail.com>
2019-12-29 23:56:36 +0800

    add api

--- route/add.go
+++ route/add.go
@@ -2,71 +2,131 @@ package route
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/liyuliang/rqueue/request"
-	//"github.com/gorilla/mux"
 	"github.com/liyuliang/rqueue/system"
 	"github.com/liyuliang/utils/format"
 	"github.com/liyuliang/utils/regex"
 	"net/url"
 	"github.com/pkg/errors"
+	"strings"
 )
 
-const URL = "url"
-const CATEGORY = "type"
+const uuid = "uuid"
+const uri = "url"
+const uris = "[]url"
+const category = "type"
 
 func add(c *gin.Context) {
 
-	post := request.Data(c.Request)
+	id, err := getPostParam(c, uuid)
+	if err != nil {
+		c.JSON(200, format.ToMapData(map[string]string{
+			"error":   err.Error() + ":" + uuid,
+			"success": "false",
+		}))
+		return
+	}
 
-	err := checkJob(post)
+	err = checkUUid(id)
+
 	if err != nil {
 		c.JSON(200, format.ToMapData(map[string]string{
 			"error":   err.Error(),
 			"success": "false",
 		}))
-		c.Abort()
+		return
 	}
 
-	key := genQueueKey(post)
-	client := system.Client()
-	r := client.RPush(key, format.ToMapData(post).ToString())
-	if r.Err() != nil {
+	cat, err := getPostParam(c, category)
+	if err != nil {
 		c.JSON(200, format.ToMapData(map[string]string{
-			"error":   r.Err().Error(),
+			"error":   err.Error() + ":" + category,
 			"success": "false",
 		}))
-	} else {
+		return
+	}
+
+	urls, err := getPostParams(c, uris)
+	if err != nil {
 		c.JSON(200, format.ToMapData(map[string]string{
-			"error":   "",
-			"success": "true",
+			"error":   err.Error() + ":" + uris,
+			"success": "false",
 		}))
+		return
 	}
-	//routeParams := mux.Vars(c.Request)
+
+	client := system.Client()
+	for _, u := range urls {
+		key := genQueueKey(cat, u)
+
+		client.RPush(key, u)
+
+
+		//队列集-总数
+		client.HSet()
+	}
+
+
+	c.JSON(200, format.ToMapData(map[string]string{
+		"error":   "",
+		"success": "true",
+	}))
 }
 
-func genQueueKey(data map[string]string) string {
-	url := data[URL]
-	category := data[CATEGORY]
-	return category + getKeyFromUrl(url)
+func checkUUid(uuid string) error {
+	//TODO 24小时 uuid, 防止重播攻击
+	return nil
 }
 
-func checkJob(data map[string]string) error {
-
-	url := data[URL]
-	category := data[CATEGORY]
-
-	key := getKeyFromUrl(url)
-
-	if url == "" {
-		return errors.New("url is required")
+func getPostParams(c *gin.Context, key string) ([]string, error) {
+	v := c.PostFormArray(key)
+	if len(v) == 0 {
+		v2 := c.PostForm(strings.Replace(key, "[]", "", -1))
+		if v2 != "" {
+			v = []string{v2}
+		}
 	}
-	if category == "" {
-		return errors.New("type is required")
+	return v, checkUris(v)
+}
+
+func getPostParam(c *gin.Context, key string) (string, error) {
+	v := c.PostForm(key)
+	return v, checkEmpty(v)
+}
+
+func genQueueKey(category, uri string) string {
+	return category + "_" + getKeyFromUrl(uri)
+}
+
+func checkEmpty(v string) error {
+	if v == "" {
+		return errors.New("missing required param")
 	}
+	return nil
+}
+
+func checkUri(uri string) error {
+
+	if uri == "" {
+		return errors.New("url is addRequired")
+	}
+
+	key := getKeyFromUrl(uri)
+
 	if key == "" {
 		return errors.New("It's not avail url")
 	}
 	return nil
+}
+
+func checkUris(uris []string) (err error) {
+	for _, uri := range uris {
+
+		err = checkUri(uri)
+		if err != nil {
+			break
+		}
+	}
+	return err
 }
 
 func getKeyFromUrl(uri string) string {
